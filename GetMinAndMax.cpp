4d4e7fd67427d66661c25bfc9cbbec9a0c7636f8
Soyn <w604408482@163.com>
2015-07-31 16:51:52 +0800

    fix

--- GetMinAndMax.cpp
+++ GetMinAndMax.cpp
@@ -12,7 +12,8 @@
 
 std :: pair<int, int> max_min( int lhs, int rhs)
 {
-        return lhs > rhs ? std :: make_pair(lhs,rhs) : std :: make_pair( rhs, lhs);
+
+    return lhs > rhs ? std :: make_pair(lhs,rhs) : std :: make_pair( rhs, lhs);
 }
 
 // at least 2 items in vector
@@ -22,11 +23,11 @@ std :: pair<int, int> find_max_min( std:: vector<int> const & sequence)
         std :: tie( max, min) = max_min( sequence[0], sequence[1]);
         for( auto it = sequence.cbegin(); it < sequence.cend() - 1; it += 1)
         {
-                    auto curr_pair = max_min(*(it), *( it + 1));
-                    max = std ::max( max, curr_pair.first), min = std :: min( min, curr_pair.second);
+            auto curr_pair = max_min(*(it), *( it + 1));
+            max = std ::max( max, curr_pair.first), min = std :: min( min, curr_pair.second);
         }
         if( sequence.size() & 0x1)  // for odd size
-                    max = std :: max( max,sequence.back()), min = std :: min( min, sequence.back());
+                max = std :: max( max,sequence.back()), min = std :: min( min, sequence.back());
 
         return std :: make_pair(max, min);
 }
