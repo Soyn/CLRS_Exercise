cd4ff3ef537222f8c6ac52fe64558765e3b1a41c
Soyn <w604408482@163.com>
2015-07-31 16:22:33 +0800

    Refactor

--- GetMinAndMax.cpp
+++ GetMinAndMax.cpp
@@ -1,42 +1,40 @@
 #include <iostream>
-#include <initializer_list>
+#include <tuple>
+#include <algorithm>
+#include <vector>
+
 //!
 //! @Brief:get the minmum and maxmum 3 * (n // 2) comparison
 //! Create by Soyn. 31/7/15.
+//!  Refactor by @Mooopy
 //!
 
-bool IsOdd( size_t n)
+
+std :: pair<int, int> max_min( int lhs, int rhs)
 {
-    return n % 2 ;
+        return lhs > rhs ? std :: make_pair(lhs,rhs) : std :: make_pair( rhs, lhs);
 }
-std::initializer_list<int> getLargerAndSmaller( int a, int b)
+
+// at least 2 items in vector
+std :: pair<int, int> find_max_min( std:: vector<int> const & sequence)
 {
-    if(a <= b){
-        return {a,b};
-    }else{
-        return {b,a};
-    }
+        int max, min;
+        std :: tie( max, min) = max_min( sequence[0], sequence[1]);
+        for( auto it = sequence.cbegin(); it < sequence.cend() - 1; it += 1)
+        {
+                    auto curr_pair = max_min(*(it), *( it + 1));
+                    max = std ::max( max, curr_pair.first), min = std :: min( min, curr_pair.second);
+        }
+        if( sequence.size() & 0x1)  // for odd size
+                    max = std :: max( max,sequence.back()), min = std :: min( min, sequence.back());
+
+        return std :: make_pair(max, min);
 }
 
 int main(void)
 {
-    int  Min, Max;
-    int a[] = {5,4,1,7,3,8,3,4,9,10};
-    int n = sizeof(a) / sizeof(a[0]);
-    for( int i = 0; i < n - 1; i += 2){
-        if( (i ==0) &&IsOdd(n)){           // initialize the Min and Max.
-            Min = Max = a[i];
-        } else{
-                auto item = getLargerAndSmaller(a[i],a[i+1]);
-                if(i == 0){
-                        Min = *(item.begin());
-                        Max = *(item.begin() + 1);
-                    }else{
-                    Min > *(item.begin()) ? Min = *(item.begin()) : Min;
-                    Max < *(item.begin() + 1) ? Max = *(item.begin() + 1) : Max;
-                    }
-        }
-    }
-           std :: cout << "Min: " << Min << " , " << "Max: " << Max << std :: endl;
-     return 0;
+        std :: vector<int> sample = {11,52,-3,-4,75,60,-199};
+        auto pair = find_max_min(sample);
+        std :: cout << "max : " << pair.first << " min : " << pair.second << std :: endl;
+        return 0;
 }
