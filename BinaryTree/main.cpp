88337883b036a34d7cd551c89bb0d6eb05282e7e
Soyn <w604408482@163.com>
2015-09-11 12:28:54 +0800

    main.cpp

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
+#include "BinaryTree.h"
+#include "Traversal_Recursion.h"
+#include "Traversal_Iteration.h"
 
-using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a[5] = {3,2,5,10,9};
+
+    node *root = BinaryTree(a,5);s
+
+    std :: cout << "PrevOrderTraversal" << std :: endl;
+    PrevOrderTraversal(root);
+    std :: cout << "MiddOrderTraversal" << std :: endl;
+    MiddOrderTraversal(root);
+    std :: cout << "PostOrderTraversal" << std :: endl;
+
+    std :: cout << "PrevOrderTraversal_Iter" << std :: endl;
+    PrevOrderTraversal_Iter(root);
+    std :: cout << "MiddOrderTraversal_Iter" << std :: endl;
+    MiddOrderTraversal_Iter(root);
+    std :: cout << "PostOrderTraversal_Iter" << std :: endl;
+    PostOrderTraversal_Iter(root);
     return 0;
 }
