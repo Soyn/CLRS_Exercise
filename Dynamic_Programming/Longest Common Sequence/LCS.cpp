ca335df30c0b14b4135e73eefad8222aad67218a
Soyn <w604408482@163.com>
2016-05-13 21:26:29 +0800

    polish

--- Dynamic_Programming/Longest Common Sequence/LCS.cpp
+++ Dynamic_Programming/Longest Common Sequence/LCS.cpp
@@ -1,88 +1,94 @@
+/********************************
+* @Author: Soyn
+* @Brief:The longest common sequence
+* @CreatedTime:24/1/16.
+********************************/
 #include <iostream>
-/*
-********************************
-* The longest common sequence
-* Created by Soyn.
-* Created Time: 24/1/16.
-********************************
-*/
+#include <memory>
+#include <string>
 
+//
+// @Brief: longest common sequence
 class LCS{
 public:
     LCS() = default;
-    LCS(const std :: string str1, const std :: string str2) : S1(str1), S2(str2)
-    {
-        S1Len = S1.length() + 1;
-        S2Len = S2.length() + 1;
-        C = new int(S1Len * S2Len);
-    }
+    LCS(const std :: string sequence_x, const std :: string sequence_y) :
+        sequence_x_(sequence_x), sequence_y_(sequence_y),
+        sequence_x_length_(sequence_x_.length()), sequence_y_length_(
+        sequence_y_.length()), record_length_table_(
+        new int[(sequence_x_length_ + 1) * (sequence_y_length_ + 1)])
+        {
+
+        }
+    void GetLength();
+    LCS(const LCS &) = delete;
+    LCS& operator=(const LCS&) = delete;
+
     void PrintLCS(int, int);
     void LCSLength();
     void PrintTable();
-    ~LCS()
-    {
-        delete C;
-    }
+    ~LCS(){}
 private:
-    std :: string S1, S2;
-    int S1Len, S2Len;
-    int *C;
+    std :: string sequence_x_, sequence_y_;
+    size_t sequence_x_length_, sequence_y_length_;
+    std :: unique_ptr<int[]> record_length_table_;
 };
 
-int main(int argc, char **argv)
+void LCS :: GetLength()
 {
-    std :: string S1("ABCBDAB"), S2("BDCABA");
-    LCS L1(S1,S2);
-    L1.LCSLength();
-    //L1.PrintLCS(7,6);
-    L1.PrintTable();
-    return 0;
+    std :: cout << "Sequence_x_length: " << sequence_x_length_ << " ";
+    std :: cout << "Sequence_y_length: " << sequence_y_length_ << " ";
 }
-
 void LCS :: LCSLength()
 {
-    char Chr1[S1Len], Chr2[S2Len];
-    for(int i = 1; i < S1Len; ++i)
-        Chr1[i] = S1[i - 1];
-    for(int i = 1; i < S2Len; ++i)
-        Chr2[i] = S2[i - 1];
+    char char_table_x[sequence_x_length_ + 1],
+         char_table_y[sequence_y_length_ + 1];
 
-    // assgin 0 to C[i][0]
-    for(int i = 0; i < S1Len; ++i){
-        C[i * S2Len] = 0;
-    }
-    // assgin 0 to C[0][i]
-    for(int i = 0; i < S2Len; ++i){
-        C[i] = 0;
-    }
+    for(auto i = 1; i < sequence_x_length_; ++i)
+        char_table_x[i] = sequence_x_[i - 1];
+
+    for(auto i = 1; i < sequence_y_length_; ++i)
+        char_table_y[i] = sequence_y_[i - 1];
+
+    for(auto i = 0; i < sequence_x_length_ * sequence_y_length_ + 2; ++i)
+        record_length_table_[i] = 0;
 
     //the procedure to get the length table of subsequence
-    int row, col;
-    for( row = 1; row < S1Len; ++row){
-        for( col = 1; col < S2Len; ++col){
-            if(Chr1[row] == Chr2[col]){
-                C[(row *S2Len + col)] = C[((row - 1) * S2Len + (col - 1))] + 1;
+    for(auto row = 1; row < sequence_x_length_ + 1 ; ++row){
+        for(auto col = 1; col < sequence_y_length_ + 1; ++col){
+            if(char_table_x[row] == char_table_y[col]){
+                record_length_table_[(row * sequence_y_length_ + col)] =
+                 record_length_table_[((row - 1) *
+                                sequence_y_length_ + (col - 1))] + 1;
             }else{
-                if(C[((row - 1) * S2Len + col)] >= C[(row * S2Len + (col - 1))]){
-                    C[(row * S2Len + col)] = C[((row - 1) * S2Len  + col)];
+                if(record_length_table_[((row - 1) * sequence_y_length_ + col)]
+            >= record_length_table_[(row * sequence_y_length_ + (col - 1))]){
+
+                    record_length_table_[(row * sequence_y_length_ + col)] =
+                record_length_table_[((row - 1) * sequence_y_length_  + col)];
+
                 }else{
-                    C[(row * S2Len + col)] = C[(row * S2Len + (col - 1))];
+
+                    record_length_table_[(row * sequence_y_length_ + col)] =
+                record_length_table_[(row * sequence_y_length_ + (col - 1))];
+
                 }
             }
         }
     }
 }
 
-void LCS :: PrintLCS(int row, int col )
+void LCS :: PrintLCS(int row, int col)
 {
-    std :: cout << S1;
+    std :: cout << sequence_x_;
     if(row ==0 || col == 0)
         return;
-    if(S1[row - 1] == S2[col - 1]){
+    if(sequence_x_[row - 1] == sequence_y_[col - 1]){
         PrintLCS(row - 1, col - 1);
-        std :: cout <<  "Row: " << row << ' ' << S1[row] << '\n';
+        std :: cout <<  "Row: " << row << ' ' << sequence_x_[row] << '\n';
     }else{
-        if(C[(row - 1) * S2Len + col] >= C[row * S2Len +(col - 1)])
+        if(record_length_table_[(row - 1) * sequence_y_length_ + col] >=
+           record_length_table_[row * sequence_y_length_ +(col - 1)])
             PrintLCS(row - 1, col);
         else
             PrintLCS(row, col - 1);
@@ -91,11 +97,23 @@ void LCS :: PrintLCS(int row, int col )
 
 void LCS :: PrintTable()
 {
-    int Count = 0;
-    for(int i = 0; i < S1Len * S2Len; ++i){
-        ++Count;
-        std :: cout << C[i] << ' ';
-        if( Count % S2Len == 0)
+    int _count = 0;
+    for(int i = 0; i < (sequence_x_length_ + 1) * (sequence_y_length_ + 1);
+        ++i){
+        ++_count;
+        std :: cout << record_length_table_[i] << ' ';
+        if( _count % sequence_y_length_ == 0)
             std :: cout << '\n';
     }
+}
+
+int main(int argc, char **argv)
+{
+    std :: string S1("ABCBDAB"), S2("BDCABA");
+    LCS L1(S1,S2);
+    L1.LCSLength();
+    //L1.PrintLCS(7,6);
+    //L1.PrintTable();
+    L1.GetLength();
+    return 0;
 }
